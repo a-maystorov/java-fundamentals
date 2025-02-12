8aa85277c1e33d35dd191b2ae45e662ce2e58e68
a-maystorov <alkin971@gmail.com>
2025-02-12 19:37:25 +0100

    feat: add validation

--- src/MortgageCalculator.java
+++ src/MortgageCalculator.java
@@ -8,16 +8,45 @@ public class MortgageCalculator {
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
 
+        int principal = 0;
+        float annualInterestRate = 0;
+        byte periodInYears = 0;
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+        while (true) {
+            System.out.print("Principal ($1K - $1M): ");
+            principal = scanner.nextInt();
 
-        System.out.print("Annual Interest Rate: ");
-        float annualInterestRate = scanner.nextFloat();
+            if (principal < 1_000 || principal > 1_000_000) {
+                System.out.println("Enter a number between 1,000 amd 1,000,000");
+                continue;
+            }
 
-        System.out.print("Period (Years): ");
-        byte periodInYears = scanner.nextByte();
+            break;
+        }
+
+        while (true) {
+            System.out.print("Annual Interest Rate: ");
+            annualInterestRate = scanner.nextFloat();
+
+            if (annualInterestRate < 0 || annualInterestRate > 30) {
+                System.out.println("Enter a number greater than 0 and less than or equal to 30");
+                continue;
+            }
+            break;
+        }
+
+        while (true) {
+            System.out.print("Period (Years): ");
+            periodInYears = scanner.nextByte();
+
+            if (periodInYears < 1 || periodInYears > 30) {
+                System.out.println("Enter a number between 1 and 30");
+                continue;
+            }
+            break;
+        }
 
         float monthlyInterestRate = (annualInterestRate / PERCENT) / MONTHS_IN_YEAR;
         int numberOfPayments = periodInYears * MONTHS_IN_YEAR;
@@ -28,5 +57,7 @@ public class MortgageCalculator {
 
         String formattedResult = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println("Mortgage: " + formattedResult);
+
+
     }
 }
