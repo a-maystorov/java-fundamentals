a4f717e02a8c798a5f14750f08c9d4634d94b23d
a-maystorov <alkin971@gmail.com>
2025-02-15 21:05:24 +0100

    refactor: improve calculator validation

--- src/MortgageCalculator.java
+++ src/MortgageCalculator.java
@@ -1,25 +1,34 @@
 public class MortgageCalculator {
-    private final static byte MONTHS_IN_YEAR = 12;
-    private final static byte PERCENT = 100;
+    private static final byte MONTHS_IN_YEAR = 12;
+    private static final byte PERCENT = 100;
 
     private final int principal;
     private final float annualInterest;
     private final byte years;
 
     public MortgageCalculator(int principal, float annualInterest, byte years) {
+        if (principal <= 0)
+            throw new IllegalArgumentException("Principal must be greater than 0");
+        if (annualInterest <= 0)
+            throw new IllegalArgumentException("Annual interest rate must be greater than 0");
+        if (years <= 0)
+            throw new IllegalArgumentException("Years must be greater than 0");
+
         this.principal = principal;
         this.annualInterest = annualInterest;
         this.years = years;
     }
 
     public double calculateBalance(short numberOfPaymentsMade) {
+        if (numberOfPaymentsMade < 0 || numberOfPaymentsMade > getNumberOfPayments())
+            throw new IllegalArgumentException("Invalid number of payments");
+
         float monthlyInterest = getMonthlyInterest();
         float numberOfPayments = getNumberOfPayments();
 
         return principal
                 * (Math.pow(1 + monthlyInterest, numberOfPayments) - Math.pow(1 + monthlyInterest, numberOfPaymentsMade))
                 / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
-
     }
 
     public double calculateMortgage() {
@@ -30,6 +39,14 @@ public class MortgageCalculator {
                 * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
                 / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
     }
+    
+    public double[] getRemainingBalances() {
+        var balances = new double[getNumberOfPayments()];
+        for (short month = 1; month <= balances.length; month++)
+            balances[month - 1] = calculateBalance(month);
+
+        return balances;
+    }
 
     private int getNumberOfPayments() {
         return years * MONTHS_IN_YEAR;
@@ -39,11 +56,4 @@ public class MortgageCalculator {
         return annualInterest / PERCENT / MONTHS_IN_YEAR;
     }
 
-    public double[] getRemainingBalances() {
-        var balances = new double[getNumberOfPayments()];
-        for (short month = 1; month <= balances.length; month++) {
-            balances[month - 1] = calculateBalance(month);
-        }
-        return balances;
-    }
 }
